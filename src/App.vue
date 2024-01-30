18a5f6da530b84649bddaf12022a3b0b770f0826
Lorena Munteanu <c_lorena-elena.munteanu@viasat.com>
2024-01-30 11:40:13 +0200

    Added CardComponent and installed vue router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template id="app">
+      <div id="app-container">
+        <CardComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardComponent from "./components/CardComponent.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    CardComponent,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app-container {
+  background-color: hsl(216, 12%, 8%);
+  height: 100vh;
+  color: white;
+  display: flex;
 }
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <main class="wrapper">
+    <section class="cardContainer">
+      <img src="../assets/icon-star.svg" alt="icon star" />
+      <h1 class="rating-title">How did we do?</h1>
+      <p class="rating-description">Please let us know how we did with your support request. All feedback is appreciated
+        to help us improve our
+        offering!</p>
+      <nav class="rating-navbar">
+            <button class="rating-link" v-for="item in items" :key="item.id" @click="handleClick(item.value)">{{ item.title }}</button>
+          </nav>
+          <h5 v-show="!selectedValue && isSubmitted" >Please select a rating</h5>
+          <button class="rating-btn" @click="handleSubmit">
+            Submit
+          </button>
+    </section>
+  </main>
+  
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  data() {
+    return {
+      name: "Card",
+      isSubmitted: false,
+      selectedValue: null,
+      items: [{
+        title: '1',
+        value: '1',
+      },
+      {
+        title: '2',
+        value: '2',
+      },
+      {
+        title: '3',
+        value: '3',
+      },
+      {
+        title: '4',
+        value: '4',
+      },
+      {
+        title: '5',
+        value: '5',
+      }
+      ]
+    }
+  },
+  methods:{
+      handleClick (value){
+        this.selectedValue = value
+      },
+      handleSubmit(){
+        this.isSubmitted = true
+        console.log("Selected value", this.selectedValue)
+      }
+    }
+};
+</script>
+
+<style lng="scss" scoped>
+@import "./style.scss";
+</style>
